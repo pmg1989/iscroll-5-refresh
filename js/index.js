4afcc2149fe3302111ea35a0e4c96ed92d6a4a3e
pmg1989 <972401854@qq.com>
2017-05-18 15:32:06 +0800

    fix vue.js load

--- js/index.js
+++ js/index.js
@@ -1,148 +1,17 @@
-var categorys = [17,18,19,2,14,15]; //每个类别的ID号
 var ir;
+var categorys = [1, 2, 3, 4, 5, 6]; //每个类别的ID号
 
-function pullDown(param){
-	/*
-	param.scroll  当前滚动的ISCROLL对象
-	param.index   当前页面的索引，从0开始
-	param.lastUpdate 上一次刷新的时间
-	*/
-	var index = param.index;
-	function callback(data){
+$(function() {
+  loaded()
 
-		if(parseInt(data.code) == 200){
-
-			for(var i=0;i<data.data.length;i++){
-				var art = data.data[i];
-				var html = parseHtml(art);
-				$('.ir-scroller').eq(index).prepend(html);
-			}
-			ir.pullDownCallBack(param); //这个一定要写，是隐藏上拉刷新提示用得
-		}else{
-			ir.pullDownCallBack(param,1); //后面的1代表没有更新的数据了，不传的话，还可以继续下拉
-		}
-
-	}
-
-	testData(callback);
-
-};
-
-function pullUp(param){
-	/*
-	param.scroll  当前滚动的ISCROLL对象
-	param.index   当前页面的索引，从0开始
-	param.lastUpdate 上一次刷新的时间
-	param.page  当前已经加载的页数
-	*/
-	var index = param.index;
-	var page = param.page;
-
-	function callback(data){
-
-		if(parseInt(data.code) == 200){
-
-			for(var i=0;i<data.data.length;i++){
-				var art = data.data[i];
-				var html = parseHtml(art);
-				$('.ir-scroller').eq(index).append(html);
-			}
-			ir.pullUpCallBack(param); //还有数据的时候用这个
-		}else{
-			ir.pullUpCallBack(param,1); //没有更多数据的时候后面多加个1就行
-		}
-
-	}
-	testData(callback);
-}
-
-function slide(index){
-	if($('.ir-scroller').eq(index).find('div.loader').length >0){
-		initData(index,1);
-	}
-}
-
-function initData(index,page){
-
-	function callback(data){
-
-		if(parseInt(data.code) == 200){
-
-			for(var i=0;i<data.data.length;i++){
-				var art = data.data[i];
-				var html = parseHtml(art);
-				$('.ir-scroller').eq(index).append(html);
-			}
-
-		}
-
-		$('.ir-scroller').eq(index).find('.loader').remove();
-
-		ir.setPage(index,1);  //设置当前页面的页数
-		ir.refresh(index); //刷新Iscroll
-	}
-
-	testData(callback);
-
-
-}
-
-function testClick(id) {
-	//alert(id);
-	// event.preventDefault()
-}
-
-$(document).on('tap',"#ir-bd-wrapper .ir-scroller li", function(e){
-	var id = $(this).data('id');
-	//alert(id);
-	location.href = "detail.html?id=" + id
+	$(document).on('tap',"#ir-bd-wrapper .ir-scroller li", function(e){
+		var id = $(this).attr('data-id');
+		alert(id);
+		//location.href = "detail.html?id=" + id
+	})
 })
-
-function parseHtml(art){
-	var html = '<li data-id="' + art.id + '"><a class="clear-fix" href="javascript:void(0)"><img src="'+art.thumbnail+'"><div class="li-body"><h3>'+art.title+'</h3><p>'+art.created_at+'</p></div></a></li>';
-	if(!art.thumbnail){
-	html = '<li><a href="/articles/show/'+art.id+'"><div class="li-body" style="width:100%"><h3>'+art.title+'</h3><p>'+art.created_at+'</p></div></a></li>';
-	}
-	return html;
-}
-
-/*模拟AJAX获取数据*/
-var idtest = 1;
-function testData(callback){
-	var article = {
-			id:1,
-			thumbnail:'http://7xn32g.com1.z0.glb.clouddn.com/_cloud_file_201653_121412_3571904.JPG?imageView2/1/w/140/h/110',
-			title:'携程商旅联手明道抢滩企业级服务市场',
-			created_at:'2016-06-01 12:22'
-		};
-	var data = new Array();
-	for (var i = 0; i < 10; i++) {
-		var article = {
-				id:idtest++,
-				thumbnail:'http://7xn32g.com1.z0.glb.clouddn.com/_cloud_file_201653_121412_3571904.JPG?imageView2/1/w/140/h/110',
-				title:'携程商旅联手明道抢滩企业级服务市场',
-				created_at:'2016-06-01 12:22'
-			};
-		data.push(article);
-	};
-
-	setTimeout(function(){
-		callback({code:200,data:data})
-	},400);
-}
-
-//如果在页面内有其他滑动冲突的时候可以绑定touch事件禁止iscroll的滑动
-/*
-$('#viewport').on('touchstart',function(){
-	ir.bdScroll.disable(); //禁止左右滑动
-})
-$('#viewport').on('touchend',function(){
-	ir.bdScroll.enable(); // 开启左右滑动
-})
-*/
 
 function loaded () {
-
 	$('.ir-scroller').each(function(index, el) {
 		$(this).append('<div class="loader"><div class="line-scale"><div></div><div></div><div></div><div></div><div></div></div><div style="margin-top:15px">加载中...</div></div>');
 	});
@@ -156,6 +25,91 @@ function loaded () {
 	slide(0)
 }
 
-$(function() {
-  loaded()
+function slide(index){
+	if($('.ir-scroller').eq(index).find('div.loader').length >0){
+		initData(index,0);
+	}
+}
+
+function initData(index,page){
+	getData({ tab: index, page: page, cid: categorys[index], type: 'init' }, function (data) {
+		if(parseInt(data.code) == 200){
+			var $irScroller = $('.ir-scroller').eq(index);
+			for(var i=0;i<data.list.length;i++){
+				var art = data.list[i];
+				var html = parseHtml(art);
+				$irScroller.append(html);
+			}
+			$('.ir-scroller').eq(index).find('.loader').remove();
+			ir.setPage(index,1);  //设置当前页面的页数
+			ir.refresh(index); //刷新Iscroll
+		}
+	});
+}
+
+function pullDown(param){
+	/*
+	param.scroll  当前滚动的ISCROLL对象
+	param.index   当前页面的索引，从0开始
+	param.lastUpdate 上一次刷新的时间
+	*/
+	var index = param.index;
+	getData({ tab: index, page: 0, cid: categorys[index], type: 'pullDown' }, function (data) {
+		if(parseInt(data.code) == 200) {
+			var $irScroller = $('.ir-scroller').eq(index);
+			$irScroller.empty();
+			for(var i=0;i<data.list.length;i++){
+				var art = data.list[i];
+				var html = parseHtml(art);
+				$irScroller.append(html);
+			}
+			ir.setPage(index,1);  //设置当前页面的页数
+			ir.pullDownCallBack(param); //还有数据的时候用这个
+		} else {
+			ir.setPage(index,1);  //设置当前页面的页数
+			ir.pullDownCallBack(param,1); //没有更多数据的时候后面多加个1就行
+		}
+	});
+};
+
+function pullUp(param){
+	/*
+	param.scroll  当前滚动的ISCROLL对象
+	param.index   当前页面的索引，从0开始
+	param.lastUpdate 上一次刷新的时间
+	param.page  当前已经加载的页数
+	*/
+	var index = param.index;
+	var page = param.page;
+	getData({ tab: index, page: page, cid: categorys[index], type: 'pullUp' }, function (data) {
+		if(parseInt(data.code) == 200) {
+			var $irScroller = $('.ir-scroller').eq(index);
+			for(var i=0;i<data.list.length;i++){
+				var art = data.list[i];
+				var html = parseHtml(art);
+				$irScroller.append(html);
+			}
+			ir.pullUpCallBack(param); //还有数据的时候用这个
+		} else {
+			ir.pullUpCallBack(param,1); //没有更多数据的时候后面多加个1就行
+		}
+	});
+}
+
+function parseHtml(art){
+	var html = '<li data-id="' + art.id + '"><a class="clear-fix" href="javascript:void(0)"><img src="'+art.thumbnail+'"><div class="li-body"><h3>'+art.title+'</h3><p>'+art.created_at+'</p></div></a></li>';
+	if(!art.thumbnail){
+	html = '<li><a href="/articles/show/'+art.id+'"><div class="li-body" style="width:100%"><h3>'+art.title+'</h3><p>'+art.created_at+'</p></div></a></li>';
+	}
+	return html;
+}
+
+//如果在页面内有其他滑动冲突的时候可以绑定touch事件禁止iscroll的滑动
+/*
+$('#viewport').on('touchstart',function(){
+	ir.bdScroll.disable(); //禁止左右滑动
 })
+$('#viewport').on('touchend',function(){
+	ir.bdScroll.enable(); // 开启左右滑动
+})
+*/
